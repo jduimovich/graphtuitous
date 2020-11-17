dd06bfdd9b0c6b072f3b473c665076d5f48d3f82
John Duimovich <jduimovich@gmail.com>
2020-11-17 18:24:32 -0500

    blue

--- app.js
+++ app.js
@@ -4,8 +4,8 @@ var path = require('path');
 const { exec } = require('child_process');
 var os = require('os');
 
-var APP_VERSION = '1.9';
-var APP_COLOUR = 'blue'; 
+var APP_VERSION = '2.0';
+var APP_COLOUR = 'green'; 
 var HOSTNAME = os.hostname();
  
 var SIM= process.env.SIM;
