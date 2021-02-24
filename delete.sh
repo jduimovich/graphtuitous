4f2dceade69594d884ea68659542ad94a991eb41
John Duimovich <jduimovich@gmail.com>
2021-02-23 19:39:06 -0500

    shell scripts

---
+++ delete.sh
@@ -0,0 +1,12 @@
+ 
+kubectl delete -f services/fib-quarkus/deploy
+kubectl delete -f services/fib-go/deploy
+kubectl delete -f services/fib-node/deploy
+kubectl delete -f services/service.yaml 
+
+
+kubectl delete -f frontend/deploy
+ 
+kubectl delete -f haproxy/deploy
+kubectl delete -f haproxy/deploy-openshift
+ 
