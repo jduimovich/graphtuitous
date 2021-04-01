3039032a8d22f5f6df83f620469ef53e6f7696f4
John Duimovich <jduimovich@gmail.com>
2021-04-01 10:16:25 -0400

    fronend test

--- compute_change.sh
+++ compute_change.sh
@@ -1,42 +1,45 @@
 
-git diff --name-only HEAD HEAD~1 > changed_directories
+DELTA=changed_directories
+git diff --name-only HEAD HEAD~1 > $DELTA
 
 export CHANGE_ANY=false
-if grep fib-go changed_directories
+if grep fib-go $DELTA
 then
     export CHANGE_FIB_GO=true
     export CHANGE_ANY=true
 else
     export CHANGE_FIB_GO=false
 fi
-if grep  fib-node changed_directories
+if grep  fib-node $DELTA
 then
     export CHANGE_FIB_NODE=true
     export CHANGE_ANY=true
 else
     export CHANGE_FIB_NODE=false
 fi
-if grep fib-quarkus changed_directories
+if grep fib-quarkus $DELTA
 then
     export CHANGE_FIB_QUARKUS=true
     export CHANGE_ANY=true
 else
     export CHANGE_FIB_QUARKUS=false
 fi
-if grep haproxy changed_directories
+if grep haproxy $DELTA
 then
     export CHANGE_HAPROXY=true
     export CHANGE_ANY=true
 else
     export CHANGE_HAPROXY=false
 fi
-if grep fib-go changed_directories
+if grep frontend $DELTA
 then
     export CHANGE_FRONTEND=true
     export CHANGE_ANY=true
 else
     export CHANGE_FRONTEND=false
 fi
+
+cat $DELTA 
 
 echo CHANGE_FIB_GO  $CHANGE_FIB_GO
 echo CHANGE_FIB_NODE  $CHANGE_FIB_NODE
