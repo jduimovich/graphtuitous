538c787d9281849cce58a79344a2c02f0f3a3aa3
John Duimovich <jduimovich@gmail.com>
2021-04-01 14:42:36 -0400

    orange

--- services/fib-go/fib.go
+++ services/fib-go/fib.go
@@ -31,7 +31,7 @@ var health *Health = &Health{
 var response *Response = &Response{
 	Hostname: "GO",
 	Version:  "1.1",
-	Colour:   "pink",
+	Colour:   "orange",
 	Count:    0,
 	Stack:    "golang",
 	Loop:     10000,
