b5a75060df5c9a60a5f6f4f295b5ca1a87ca4e65
John Duimovich <jduimovich@gmail.com>
2021-04-02 08:45:31 -0400

    remove digest from pull requests

--- choose-registry.cmd
+++ choose-registry.cmd
@@ -1,19 +1,26 @@
 
 @echo off
 
-rem if using dockerhub 
+set REG=%1
+IF "%REG%"=="" (GOTO :default_docker) 
+IF "%REG%"=="docker.io" (GOTO :default_docker) 
+IF "%REG%"=="quay.io" (GOTO :default_quay) 
 
+:default_docker
 set REGISTRY_USER=jduimovich
 set REGISTRY=docker.io
 set USE_DOCKER=true 
 set REGISTRY_PASSWORD=%MY_DOCKER_PW%
+goto :done_init
 
-rem if using quay 
-rem set REGISTRY_USER=jduimovich0
-rem set REGISTRY=quay.io
-rem set USE_DOCKER=false 
-rem set REGISTRY_PASSWORD=%MY_QUAY_PW% 
+:default_quay 
+set REGISTRY_USER=jduimovich0
+set REGISTRY=quay.io
+set USE_DOCKER=false 
+set REGISTRY_PASSWORD=%MY_QUAY_PW% 
+goto :done_init
 
+:done_init
 IF "%REGISTRY_PASSWORD%"=="" (GOTO :missing_reg_token) 
 
 oc whoami --show-server >openshift.server
@@ -25,9 +32,12 @@ rem echo  The token is  /%LOGIN%/
  
 yq w -i .github\workflows\build-all-and-deploy.yml env.REGISTRY %REGISTRY%/%REGISTRY_USER%
 yq w -i .github\workflows\build-all-and-deploy.yml env.REGISTRY_USER %REGISTRY_USER%
+yq w -i .github\workflows\build-all-and-deploy.yml env.REGISTRY_ROOT %REGISTRY%
 
+echo Workflow is set to the following registry, and user 
 yq r .github\workflows\build-all-and-deploy.yml env.REGISTRY 
 yq r .github\workflows\build-all-and-deploy.yml env.REGISTRY_USER  
+yq r .github\workflows\build-all-and-deploy.yml env.REGISTRY_ROOT   
 
 gh secret set OPENSHIFT_SERVER -b %SERVER%
 gh secret set OPENSHIFT_TOKEN -b %LOGIN%
@@ -35,7 +45,5 @@ gh secret set REGISTRY_PASSWORD -b %REGISTRY_PASSWORD%
 goto :end
 
 :missing_reg_token
-echo missing registry token - choose quay.io or docker.io
-
-echo %REGISTRY_PASSWORD%
+echo missing registry password - set env MY_DOCKER_PW pr MY_QUAY_PW before calling this script
 :end
