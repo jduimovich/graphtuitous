06d20a59fb741189c92de1788a0096384bc09529
John Duimovich <jduimovich@gmail.com>
2021-04-06 11:29:18 -0400

    fix secret script

--- choose-registry.cmd
+++ choose-registry.cmd
@@ -23,12 +23,18 @@ goto :done_init
 :done_init
 IF "%REGISTRY_PASSWORD%"=="" (GOTO :missing_reg_token) 
 
+oc whoami --show-token >openshift.login.token
+set /P LOGINQ=< openshift.login.token
+del openshift.login.token
+Set LOGIN=%LOGINQ:"=%
+
 oc whoami --show-server >openshift.server
 set /P SERVERQ=< openshift.server
 del openshift.server
 Set SERVER=%SERVERQ:"=%
 rem echo  The server is /%SERVER%/
-rem echo  The token is  /%LOGIN%/
+rem echo  The token is  /%LOGIN%/ 
+
  
 yq w -i .github\workflows\build-all-and-deploy.yml env.REGISTRY %REGISTRY%/%REGISTRY_USER%
 yq w -i .github\workflows\build-all-and-deploy.yml env.REGISTRY_USER %REGISTRY_USER%
