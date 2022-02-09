b485b03a1210a0029769b3d2f7d31208e2dfd4d5
John Duimovich <jduimovich@gmail.com>
2022-02-09 15:36:56 -0500

    incremental dev for go

--- services/fib-go/dev.cmd
+++ services/fib-go/dev.cmd
@@ -1,5 +1,6 @@
 cd %~dp0 
+go get -u github.com/cosmtrek/air
 set PORT=8082
-go run fib.go
+air
 
 
--- services/fib-go/dev.sh
+++ services/fib-go/dev.sh
@@ -1,7 +1,8 @@
 script_dir=$(dirname "$0") 
 cd $script_dir 
 echo $(pwd)
+go get -u github.com/cosmtrek/air
 export PORT=8082
-go run fib.go
+air
 
 
