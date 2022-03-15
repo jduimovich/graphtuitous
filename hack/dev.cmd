98df7df1420b2feb66b6d5eee47e05af5327b88f
John Duimovich <jduimovich@gmail.com>
2022-03-15 15:46:08 -0400

    /min

--- hack/dev.cmd
+++ hack/dev.cmd
@@ -4,12 +4,12 @@ set CURRENT_DIR=%cd%
 set SCRIPT_DIR=%~dp0
 cd %SCRIPT_DIR%\..
 
-start services\fib-quarkus\dev.cmd
-start services\fib-node\dev.cmd
-start services\fib-go\dev.cmd
+start /min services\fib-quarkus\dev.cmd
+start /min services\fib-node\dev.cmd
+start /min services\fib-go\dev.cmd
  
-start services\frontend\dev.cmd 
-start services\haproxy\dev.cmd 
+start /min services\frontend\dev.cmd 
+start /min services\haproxy\dev.cmd 
 
 start http://localhost:8888
 
