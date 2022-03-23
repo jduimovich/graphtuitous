56b1cb8c405daebd4154f8f7039a7e5e30df5992
John Duimovich <jduimovich@gmail.com>
2022-03-23 15:48:49 -0400

    make ns first

--- hack/deploy.cmd
+++ hack/deploy.cmd
@@ -2,5 +2,6 @@
 
 set CURRENT_DIR=%cd%
 set SCRIPT_DIR=%~dp0
-
+oc new-project graphtuitous
+oc project graphtuitous
 oc apply -R -f %SCRIPT_DIR%\..\deploy   
--- hack/deploy.sh
+++ hack/deploy.sh
@@ -1,4 +1,6 @@
 script_dir=$(dirname "$0")
 cd $script_dir/..
 
+oc new-project graphtuitous
+oc project graphtuitous
 oc apply -R -f $script_dir/../deploy    
