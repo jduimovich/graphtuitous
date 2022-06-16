26847c16cead263751730a90a0ca859f1053d669
John Duimovich <jduimovich@gmail.com>
2022-06-16 07:21:54 -0400

    Update fib.go

--- services/fib-go/fib.go
+++ services/fib-go/fib.go
@@ -31,7 +31,7 @@ var health *Health = &Health{
 var response *Response = &Response{
 	Hostname: "GO",
 	Version:  "1.1",
-	Colour:   "blue",
+	Colour:   "green",
 	Count:    0,
 	Stack:    "golang",
 	Loop:     10000,
